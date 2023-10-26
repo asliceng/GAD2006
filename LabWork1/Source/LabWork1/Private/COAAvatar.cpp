9e8509020327d9bd1213ade1fbfcf66cd25fdb59
asliceng <asli.cengiz@bahcesehir.edu.tr>
2023-10-26 13:12:02 +0300

    Labwork1 committed

---
+++ LabWork1/Source/LabWork1/Private/COAAvatar.cpp
@@ -0,0 +1,145 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "COAAvatar.h"
+
+ACOAAvatar::ACOAAvatar():
+RunningSpeed(600.0f),
+MaxStamina(100.0f),
+Stamina(0.0f),
+StaminaGainRate(15.0f),
+StaminaDrainRate(60.0f)
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->TargetArmLength = 300.0f;
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+
+	SpringArm->bUsePawnControlRotation = true;
+	Camera->bUsePawnControlRotation = false;
+	bUseControllerRotationYaw = false;
+}
+
+void ACOAAvatar::BeginPlay()
+{
+	Super::BeginPlay();
+
+	MaxStamina = 100.0f;
+	Stamina = MaxStamina;
+	bStaminaDrained = false;
+}
+
+void ACOAAvatar::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	/*UpdateMovementParams(Stamina);
+	if (!bRunning)
+	{
+		Stamina = FMath::Min(MaxStamina, Stamina + StaminaGainRate * DeltaTime);
+	}
+	else if (!bStaminaDrained && GetCharacterMovement()->MovementMode == EMovementMode::MOVE_Walking)
+	{
+		Stamina = FMath::Max(0, Stamina - StaminaDrainRate * DeltaTime);
+	}*/
+
+	if (GetCharacterMovement() -> MovementMode == EMovementMode::MOVE_Walking)
+	{
+		if (bRunning && !bStaminaDrained)
+		{
+			if (!GetCharacterMovement()->Velocity.IsNearlyZero(0.01f))
+			{
+				Stamina = FMath::Max(0, Stamina - StaminaDrainRate * DeltaTime);
+				if (Stamina == 0)
+				{
+					bStaminaDrained = true;
+					UpdateMovementParams();
+				}
+			}			
+		}
+		else
+		{
+			Stamina = FMath::Min(MaxStamina, Stamina + StaminaGainRate * DeltaTime);
+			if (Stamina >= MaxStamina)
+			{
+				bStaminaDrained = false;
+				UpdateMovementParams();
+			}
+		}
+	}
+
+	GEngine->AddOnScreenDebugMessage(0, 500.0f, FColor::Purple, FString::Printf(TEXT("Stamina: %f"), Stamina));
+
+	UE_LOG(LogTemp, Warning, TEXT("Stamina : %f"), Stamina);
+}
+
+void ACOAAvatar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Turn", this, &ACharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &ACharacter::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ACOAAvatar::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ACOAAvatar::MoveRight);
+
+	PlayerInputComponent->BindAction("Run", EInputEvent::IE_Pressed, this, &ACOAAvatar::RunPressed);
+	PlayerInputComponent->BindAction("Run", EInputEvent::IE_Released, this, &ACOAAvatar::RunReleased);
+
+}
+
+void ACOAAvatar::MoveForward(float Amount)
+{
+	FRotator ControlRotation = GetControlRotation();
+	FVector ForwardDirection = FRotationMatrix(FRotator(0.0f, ControlRotation.Yaw, 0.0f)).GetUnitAxis(EAxis::X);
+
+	AddMovementInput(ForwardDirection, Amount);
+}
+
+void ACOAAvatar::MoveRight(float Amount)
+{
+	FRotator ControlRotation = GetControlRotation();
+	FVector RightDirection = FRotationMatrix(FRotator(0.0f, ControlRotation.Yaw, 0.0f)).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(RightDirection, Amount);
+}
+
+void ACOAAvatar::RunPressed()
+{
+	/*if (!bStaminaDrained)
+	{
+		GetCharacterMovement()->MaxWalkSpeed = RunningSpeed;
+		bRunning = true;
+	}*/
+	bRunning = true;
+	UpdateMovementParams();
+}
+
+void ACOAAvatar::RunReleased()
+{
+	/*GetCharacterMovement()->MaxWalkSpeed = WalkingSpeed;
+	bRunning = false;*/
+	bRunning = false;
+	UpdateMovementParams();
+}
+
+void ACOAAvatar::UpdateMovementParams()
+{
+	GetCharacterMovement()->MaxWalkSpeed =
+		bRunning && !bStaminaDrained ? RunningSpeed : WalkingSpeed;
+}
+
+//void ACOAAvatar::UpdateMovementParams(float CurrentStamina)
+//{
+//	if (bStaminaDrained && Stamina == MaxStamina)
+//	{
+//		bStaminaDrained = false;
+//	}
+//	else if (!bStaminaDrained && CurrentStamina == 0)
+//	{
+//		bStaminaDrained = true;
+//		RunReleased();
+//	}
+//}
---
+++ LabWork1/Source/LabWork1/Private/COAAvatar.h
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "COABaseCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "COAAvatar.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class ACOAAvatar : public ACOABaseCharacter
+{
+	GENERATED_BODY()
+
+
+public:
+	ACOAAvatar();
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "COA")
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "COA")
+	UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "COA")
+	float RunningSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "COA")
+	float Stamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "COA")
+	float MaxStamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "COA")
+	float StaminaGainRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "COA")
+	float StaminaDrainRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "COA")
+	bool bStaminaDrained;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "COA")
+	bool bRunning;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	void MoveForward(float Amount);
+	void MoveRight(float Amount);
+
+	void RunPressed();
+	void RunReleased();
+
+	//void UpdateMovementParams(float CurrentStamina);
+	void UpdateMovementParams();
+	
+};
