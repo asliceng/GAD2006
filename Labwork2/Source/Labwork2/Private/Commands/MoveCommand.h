165185c672f4f6365cef33a17bc1b88859ce6d6c
asliceng <asli.cengiz@bahcesehir.edu.tr>
2023-11-19 20:55:06 +0300

    Labwork2 completed.

---
+++ Labwork2/Source/Labwork2/Private/Commands/MoveCommand.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Commands/Command.h"
+
+/**
+ * 
+ */
+class MoveCommand : public Command
+{
+public:
+	MoveCommand(FSGridPosition Src, FSGridPosition Dst);
+	~MoveCommand();
+	virtual void Execute() override;
+
+	virtual void Revert() override;
+
+
+public:
+	FSGridPosition Source, Destination;
+};
---
+++ Labwork2/Source/Labwork2/Private/TBPlayerController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TBPlayerController.h"
+
+ATBPlayerController::ATBPlayerController()
+{
+}
+
+void ATBPlayerController::BeginPlay()
+{
+	bEnableClickEvents = true;
+	bShowMouseCursor = true;
+	ClickEventKeys.AddUnique(EKeys::RightMouseButton);
+}
+
+void ATBPlayerController::OnActorClicked(AActor* Actor, FKey key)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OnClicked: %s - %s"), *Actor->GetActorLabel(), *key.ToString());
+	if (GameManager) GameManager->OnActorClicked(Actor, key);
+}
+
+void ATBPlayerController::OnBackspaceClicked(AActor* Actor, FKey key)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OnBackspace: %s - %s"), *Actor->GetActorLabel(), *key.ToString());
+	if (GameManager) GameManager->OnBackspaceClicked(Actor, key);
+}
